63da45463b2b563eb0db202ba6d156b3060c062d
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2022-05-23 19:54:29 +0530

    Initial commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyDSWigw8IVDA-q8rqLwe2nCAwZQBlMkAVM",
+  authDomain: "fiver-ecom-app.firebaseapp.com",
+  databaseURL: "https://fiver-ecom-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fiver-ecom-app",
+  storageBucket: "fiver-ecom-app.appspot.com",
+  messagingSenderId: "22732110456",
+};
